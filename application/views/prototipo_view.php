a23828c71a699fdf99d9bd4a461c0ae8a9b34ada
juanspaez <juanpaez@unicauca.edu.co>
2018-10-09 00:20:56 -0500

    juanspaez
    Agregación de íconos faltantes y columna derecha en el lienzo de prototipos para los procesos.

--- application/views/prototipo_view.php
+++ application/views/prototipo_view.php
@@ -4,16 +4,18 @@
 <meta charset="utf-8">
 
 <!--hoja de estilos para los procesos.-->
-<link rel="stylesheet" href="/ProyectoMerlin/assets/css/style_prototipos.css" type="text/css">
+<link rel="stylesheet" href="/levantamientorequisitos/assets/css/style_prototipos.css" type="text/css">
 <link rel="stylesheet" href="https://cdn.materialdesignicons.com/2.7.94/css/materialdesignicons.min.css">
 
 <div id="container" style="width: 100%; padding: 1%;">
     <h4>
         <p class="text-center">
-            Prototipado para el proceso <?php echo($proceso->nombre);?>
+            PROTOTIPO - PROCESO: <?php echo($proceso->nombre);?>
         </p>
     </h4>
     <div id="prototipo_container">
+
+        <!--Barra lateral izquierda -->
         <div class="left-bar">
             <div class="text-center">
                 <a href="#" onclick="return false">
@@ -45,6 +47,14 @@
                     </span>
                 </a>
             </div>
+
+            <div class="text-center">
+                <a href="#" onclick="return false">
+                    <span class="mdi mdi-eraser" style="font-size: 2.5em;">
+                    </span>
+                </a>
+            </div>
+
             <div class="text-center">
                 <a href="#" onclick="return false">
                     <span class="mdi mdi-chart-pie" style="font-size: 2.5em;">
@@ -52,5 +62,54 @@
                 </a>
             </div>
         </div>
+
+        <div class="r-bar">
+            <div class="text-center">
+                <a href="#" onclick="return false">
+                    <span class="mdi mdi-check-outline" style="font-size: 2.5em;">
+                    </span>
+                </a>
+            </div>
+            <div class="text-center">
+                <a href="#" onclick="return false">
+                    <span class="mdi mdi-water" style="font-size: 2.5em;">
+                    </span>
+                </a>
+            </div>
+            <div class="text-center">
+                <a href="#" onclick="return false">
+                    <span class="mdi mdi-pencil" style="font-size: 2.5em;">
+                    </span>
+                </a>
+            </div>
+            <div class="text-center">
+                <a href="#" onclick="return false">
+                    <span class="mdi mdi-heart-outline" style="font-size: 2.5em;">
+                    </span>
+                </a>
+            </div>
+            <div class="text-center">
+                <a href="#" onclick="return false">
+                    <span class="mdi mdi-security-lock" style="font-size: 2.5em;">
+                    </span>
+                </a>
+            </div>
+
+            <div class="text-center">
+                <a href="#" onclick="return false">
+                    <span class="mdi mdi-wrench" style="font-size: 2.5em;">
+                    </span>
+                </a>
+            </div>
+
+            <div class="text-center">
+                <a href="#" onclick="return false">
+                    <span class="mdi mdi-toolbox-outline" style="font-size: 2.5em;">
+                    </span>
+                </a>
+            </div>
+        </div>
+
     </div>
+
 </div>
