e0e9c7df86aa8d1a7579b55d0f6cb9d3f609cc7f
camilol2409 <camilol@unicauca.edu.co>
2019-02-25 10:49:00 -0500

    Caracteristicas y preguntas

---
+++ application/views/head/nav.php
@@ -0,0 +1,93 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.0/jquery-confirm.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.0/jquery-confirm.min.js"></script>
+<nav class="navbar navbar-light  bg-info">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">Merlin</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="<?php echo base_url(); ?>ProcesoController">Procesos</a></li>
+            <!--<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="<?php echo base_url(); ?>ProcesoController">Procesos<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#"></a></li>
+                </ul>
+            </li>-->
+            <li><a href="<?php echo base_url(); ?>RolGeneralController">Roles</a></li>
+            <li><a href="<?php echo base_url(); ?>NormativaController">Normativas</a></li>
+            <li><a href="<?php echo base_url(); ?>InterfazController">Interfaces</a></li>
+            <!-- <li><a href="<?php echo base_url(); ?>CaracteristicaController">RNF</a></li> -->
+            <li><a href="<?php echo base_url(); ?>CaracteristicaController">Configuración</a></li>
+            <li><a href="ReporteController/generarPDF" onclick="showSuccessReport()">Generar Reporte PDF</a></li>      
+        </ul>
+        <ul class="nav navbar-nav navbar-right" style="margin-right: 0px;">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> <?php echo $username; ?></a></li>
+            <li><a href="#" onclick="cerrarSesion()"><span class="glyphicon glyphicon-log-out"></span> Salir</a></li>
+        </ul>
+    </div>
+</nav>
+
+
+</div>
+
+
+<script>
+    
+    function showSuccessReport() {
+        $.alert({
+                        type: 'green',
+                        icon: 'glyphicon glyphicon-warning',
+                        title: 'Exito!',
+                        content: 'Reporte PDF Creado exitosamente',
+                    });
+    }
+
+    function cerrarSesion() {
+        $.ajax({
+            url: "<?php echo base_url();?>Login/cerrar",
+            type: "POST",
+            dataType: "json",
+            data: {
+            },
+            beforeSend: function () {
+                $("#loader").show();
+            },
+            success: function (data) {
+                $("#loader").hide();
+                window.location.href = "<?php echo base_url();?>";
+            },
+            error: function (response) {
+                $("#loader").hide();
+            }
+        });
+    }
+
+    function reporteCont() {    
+
+       $.post("ReporteController/generarPDF");
+        $.alert({
+                        type: 'green',
+                        icon: 'glyphicon glyphicon-warning',
+                        title: 'Exito!',
+                        content: 'Reporte PDF Creado exitosamente',
+                    });
+
+            /*function (data) {
+                if (data === "exito") {
+                    $.alert({
+                        type: 'orange',
+                        icon: 'glyphicon glyphicon-warning',
+                        title: 'Advertencia!',
+                        content: 'Reporte PDF Creado exitosamente',
+                    });
+                } else {
+                        $.alert({
+                            type: 'red',
+                            icon: 'glyphicon glyphicon-ok',
+                            title: 'Error!',
+                            content: 'No se pudo realizar el Registro...',
+                        });
+                    }
+    },"json");*/
+}
+</script>
+
---
+++ assets/js/prototipo.js
@@ -0,0 +1,198 @@
+    
+
+ var tiempo=new Date();
+ var segundos=tiempo.getTime();
+ var borrador=1;
+
+ 
+  /*$( function() {
+    $( ".reformable" ).resizable();
+    $( ".reformable" ).draggable();
+  } );
+ */
+ function imagenes(opcion)
+ {
+    var i=1;
+    var imgs = document.createElement("img");
+    var divs = document.createElement("div");
+    divs.className="ui-widget-content reformable";
+    var element = document.getElementById("division1");
+    element.appendChild(divs);
+    imgs.className="ui-widget-header";
+    if(opcion==1)
+      imgs.src=urll+"circulo.png";
+    else if(opcion==2)
+      imgs.src=urll+"cuadrado.png";
+    else if(opcion==3)
+      imgs.src=urll+"triangulo.png";
+    //imgs.title=""+segundos;
+    divs.id=""+segundos;
+    divs.setAttribute('onclick','borrar(this.id)');
+    divs.appendChild(imgs);
+
+    $( ".reformable" ).resizable();
+    $( ".reformable" ).draggable();
+    segundos=segundos+1;
+    
+
+ }
+
+
+ function etiquetas(opcion)
+ {
+    var i=1;
+    var imgs = document.createElement("img");
+    var divs = document.createElement("div");
+    divs.className="ui-widget-content movible";
+    var element = document.getElementById("division1");
+    element.appendChild(divs);
+    imgs.className="ui-widget-header";
+    if(opcion==1)
+      imgs.src=urll+"check.png";
+    else if(opcion==2)
+      imgs.src=urll+"llave.png";
+    else if(opcion==3)
+      imgs.src=urll+"lock.png";
+    else if(opcion==4)
+      imgs.src=urll+"corazon.png";
+    imgs.title="etiqueta"+"\n"+"encargado";
+    divs.id=""+segundos;
+    divs.setAttribute('onclick','borrar(this.id)');
+    divs.setAttribute('ondblclick','nuevaEtiqueta(this.id)');
+    divs.appendChild(imgs);
+
+    $( ".movible" ).draggable();
+    segundos=segundos+1;
+    
+
+ }
+ function nuevaEtiqueta(id)
+ {
+      var etiqueta = document.getElementById(id).firstChild;
+      if(etiqueta.title)
+      {
+
+
+
+
+        //alert(etiqueta.title);
+        var arr = etiqueta.title.split("\n");
+        var valor=document.getElementById("valor_etiqueta");
+        var encargado=document.getElementById("valor_encargado");
+        var valid=document.getElementById("valor_id"); 
+
+//
+$.confirm({
+                            title: 'Etiqueta',
+                            content: 'VALOR DE LA ETIQUETA:'+arr[0]+'<br> ENCARGAD@:'+arr[1]+'<br> Desea modificarla????',
+                            icon: 'fa fa-question-circle',
+                            animation: 'scale',
+                            closeAnimation: 'scale',
+                            opacity: 0.5,
+                            buttons: {
+                                'confirm': {
+                                    text: 'SI',
+                                    btnClass: 'btn-blue',
+                                    action: function () {
+                                        valid.setAttribute('value',id);
+                                        $("#modalEtiqueta").modal();
+                                    }
+                                },
+                                no: function () {
+                                    //$.alert('you clicked on <strong>cancel</strong>');
+                                },
+                            }
+                        });
+
+//
+
+
+        
+      } 
+      //var element=document.getElementById("valor_encargado");
+      //element.setAttribute('value','netflix special');
+      
+ }
+ function registrarEtiqueta()
+ {
+    var etiqueta = document.getElementById($("#valor_id").val()).firstChild;
+    etiqueta.setAttribute('title',$("#valor_etiqueta").val()+"\n"+$("#valor_encargado").val());
+ $("#modal_etiqueta").on("hidden.bs.modal", function(){
+    $(".modal-body").html("");
+});
+    //alert($("#valor_id").val());
+
+ }
+
+ function borrar(titulo)
+ {
+   
+  if(borrador==-1)
+  {
+    
+    var element = document.getElementById(titulo);
+    var bodyRect = document.body.getBoundingClientRect(),
+    elemRect = element.getBoundingClientRect(),
+    offset   = elemRect.left - bodyRect.left,
+    tamanio=elemRect.width;
+
+alert('Element is ' + tamanio + ' vertical pixels from <body>');
+    element.parentNode.removeChild(element);
+    
+  }
+ }
+
+ function setBorrar()
+ {
+  //var element = document.getElementById("trash");
+  //document.write(document.getElementById("sel").innerHTML);
+
+
+
+  
+  if(borrador==-1)
+  {
+      document.getElementById("sel").innerHTML = "Seleccion Normal";
+      //element.src="basura.png";   
+  }
+  else
+  {
+      document.getElementById("sel").innerHTML = "Seleccion BOrrador!!!!";
+      //element.src="basura2.png";
+  }
+  borrador=borrador*-1;
+    
+
+ }
+function guardar()
+{
+    var listado="";
+    var a =document.getElementById('division1').getElementsByTagName('div');
+    var bodyRect = document.body.getBoundingClientRect();
+    for (var i=0; i < a.length; i++ )
+    {
+          
+          if(a[i].firstChild)
+          {
+            listado=listado+"*";
+            var elemRect = a[i].getBoundingClientRect(),
+            pathh=a[i].firstChild.src,
+            x   = elemRect.left - bodyRect.left,
+            y   = elemRect.top - bodyRect.top,
+            ancho=elemRect.width,
+            alto=elemRect.height;
+            arraysource=pathh.split("/");
+            source=arraysource[arraysource.length-1];
+            listado=listado+source+'/';
+            listado=listado+ancho+'/'+alto+'/'+x+'/'+y;
+            if(a[i].firstChild.title)
+              listado=listado+'+'+a[i].firstChild.title;
+            
+
+
+            
+          }
+    
+    }
+          alert(listado);
+}
